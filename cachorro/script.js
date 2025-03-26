612fccad0b3ceadbaff7ba37907560173916d8f3
FernandaNascimentoDaSilva <fn94695@gmail.com>
2025-03-26 15:54:44 -0300

    Update script.js

--- cachorro/script.js
+++ cachorro/script.js
@@ -8,31 +8,27 @@ function carregarFotosDeCachorros() {
                 if (data.status === "success") {
                     let imgTag = document.createElement("img");
                     imgTag.src = data.message;
-                    imgTag.classList.add("fotoDeCachorro"); // Adiciona a classe CSS
+                    imgTag.classList.add("fotoDeCachorro"); 
                     galeria.appendChild(imgTag);
                 }
             });
     }
-
-    // Chama a função para atualizar imagens automaticamente
-    setInterval(atualizarImagemAleatoria, 3000); // A cada 5 segundos
+    setInterval(atualizarImagemAleatoria, 3000); 
 }
 
-// Atualiza apenas UMA imagem aleatória
 function atualizarImagemAleatoria() {
     let imagens = document.querySelectorAll(".fotoDeCachorro");
 
     if (imagens.length > 0) {
-        let indexAleatorio = Math.floor(Math.random() * imagens.length); // Escolhe uma imagem aleatória
+        let indexAleatorio = Math.floor(Math.random() * imagens.length); 
         fetch("https://dog.ceo/api/breeds/image/random")
             .then(response => response.json())
             .then(data => {
                 if (data.status === "success") {
-                    imagens[indexAleatorio].src = data.message; // Atualiza apenas essa imagem
+                    imagens[indexAleatorio].src = data.message; 
                 }
             });
     }
 }
 
-// Inicia o carregamento das imagens ao abrir a página
 window.onload = carregarFotosDeCachorros;
